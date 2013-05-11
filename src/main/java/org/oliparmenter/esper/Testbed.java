8282b571537852fe0b6095740cd60527d838f116
oliparmenter@gmail.com <oliparmenter@gmail.com>
2013-05-11 18:14:05 +0100

    Add logging

--- src/main/java/org/oliparmenter/esper/Testbed.java
+++ src/main/java/org/oliparmenter/esper/Testbed.java
@@ -1,11 +1,15 @@
 package org.oliparmenter.esper;
 
+import org.apache.log4j.Logger;
+
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
 
 public class Testbed {
 
+	private static final Logger logger = Logger.getLogger(Testbed.class);;
+	
 	/**
 	 * @param args
 	 * @throws InterruptedException 
@@ -13,6 +17,7 @@ public class Testbed {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+		logger.debug("Starting ESPER test");
 		
 		EPServiceProvider epService = EPServiceProviderManager.getDefaultProvider();
 		String expression = "select avg(mid) from org.oliparmenter.esper.Tick.win:time(15 sec)";
